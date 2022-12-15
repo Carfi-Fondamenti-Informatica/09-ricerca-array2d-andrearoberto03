464da484180eecb311c004048ddc29ba7c2eb00c
andrearoberto03 <116633548+andrearoberto03@users.noreply.github.com>
2022-12-15 18:39:09 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,4 @@
-#include "lib.h"
-bool posizione(char array1[10][20], char array2[20], int&numero){
-    for(int i=0; i<10; i++){
-        for(int k=0; k<20; k++){
-            if(array2[k] != array1[i][k]){
-                k=20;
-            }else if(k==19){
-                numero=i;
-                return true;
-            }
-        }
-    }return false;
-}
+#ifndef CLIONPROJECTS_LIB_H
+#define CLIONPROJECTS_LIB_H
+bool posizione(char array1[10][20], char array2[20], int &numero);
+#endif //CLIONPROJECTS_LIB_H
