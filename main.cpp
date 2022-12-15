70ae1de5c3593f1f98bf7e824964cd8e8112140d
andrearoberto03 <116633548+andrearoberto03@users.noreply.github.com>
2022-12-15 18:27:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char array1[10][20];
+    char array2[20];
+    int numero=0;
+    for(int i=0; i<10; i++){
+        for(int k=0; k<20; k++){
+            array1[i][k]=" ";
+            array2[k]=" ";
+        }
+    }
+    for(int i=0; i<10; i++){
+        cin >> array1[i];
+    }
+    int numero=0;
+    if(posizione(array1, array2, numero)){
+        cout << numero;
+    }else{
+        cout << "non presente";
+    }
+    return 0;
 }
