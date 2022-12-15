b29d8693a5abeeb7aa589e5a72f44a5f1ee05823
andrearoberto03 <116633548+andrearoberto03@users.noreply.github.com>
2022-12-15 18:28:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include "lib.h"
+bool posizione(char array1[10][20], char array2[20], int&numero){
+    for(int i=0; i<10; i++){
+        for(int k=0; k<20; k++){
+            if(array2[k] != array1[i][k]){
+                k=20;
+            }else if(k==19){
+                numero=i;
+                return true;
+            }
+        }
+    }return false;
+}
